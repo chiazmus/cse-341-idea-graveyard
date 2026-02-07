2b85d137b441a949cb08056222c5b52ef2ea4752
chiazmus <andrew.burnah@gmail.com>
2026-02-07 13:05:11 -0700

    fixing for render deploy

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "Contacts API",
     description: "Contacts API",
   },
-  host: "localhost:3000",
-  schemes: ["http", "https"],
+  host: "cse-341-idea-graveyard.onrender.com",
+  schemes: ["https", "http"],
 };
 
 const outputFile = "./swagger.json";
